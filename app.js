4720db1d4da014c68c24a9fb5d5ed9b46afb0246
Rakibul2580 <rh565153@gmail.com>
2022-09-11 13:43:57 +0300

    not work

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+alert('This website is a not a complete')
 const caresDisplya = () => {
     const cards = document.getElementById('all-card')
     data.forEach(data => {
-        const {q, a} = data
+        console.log(data)
+        const {q, a, h} = data
         const div = document.createElement ('div')
         div.innerHTML = `
-        <div class="card w-100 bg-base-100 shadow-xl">
+        <div class="card w-100 bg-base-100 shadow-xl h-full">
             <div class="card-body">
+                <h2 class="card-title justify-center mb-3">${h}</h2>
                 <h2 class="card-title">${q}</h2>
             <div class="card-actions justify-center mt-10">
-                <label onclick="modal('${q}', '${a}')" for="my-modal-4" class="btn btn-primary modal-button">open modal</label>
+                <label onclick="modal('${q}', '${a}')" for="my-modal-4" class="btn btn-primary modal-button">open ans</label>
             </div>
             </div>
         </div>
@@ -33,3 +36,20 @@ const modal = (q, a) =>{
     </label>
     `
 }
+
+// const modalBan = (q, a, h) =>{
+//     console.log(q)
+//     console.log(a)
+//     const modalInfo = document.getElementById('modal-info-ban')
+//     modalInfo.innerHTML = `
+//     <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+//     <div class="modal">
+//     <div class="modal-box relative">
+//         <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+//         <h3 class="text-lg font-bold">${h}</h3>
+//         <h3 class="text-lg font-bold">${q}</h3>
+//         <p class="py-4">${a}</p>
+//     </div>
+//     </div>
+//     `
+// }
